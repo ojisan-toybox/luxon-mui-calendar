e14c341c2673e6da352ecb33abb58ee6ca0e8cca
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-13 21:18:42 +0900

    create app

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "./app";
+
+const Root = () => {
+  return <App></App>;
+};
+
+ReactDOM.render(<Root></Root>, document.getElementById("root"));
