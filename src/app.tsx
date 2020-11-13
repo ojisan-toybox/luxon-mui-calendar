4a0a3449db83023b87d0c7cc2a2fee3c536e6006
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-13 21:25:49 +0900

    add picker

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,20 @@
 import * as React from "react";
+import LuxonUtils from "@date-io/luxon"; // choose your lib
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
 
 export const App = () => {
-  return <div>hello</div>;
+  const [selectedDate, handleDateChange] = React.useState(new Date());
+
+  return (
+    <MuiPickersUtilsProvider utils={LuxonUtils}>
+      <DatePicker value={selectedDate} onChange={handleDateChange} />
+      <TimePicker value={selectedDate} onChange={handleDateChange} />
+      <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+    </MuiPickersUtilsProvider>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import LuxonUtils from "@date-io/luxon";
 
 import { App } from "./app";
 
 const Root = () => {
-  return <App></App>;
+  return (
+    <MuiPickersUtilsProvider utils={LuxonUtils}>
+      <App />
+    </MuiPickersUtilsProvider>
+  );
 };
 
 ReactDOM.render(<Root></Root>, document.getElementById("root"));
