d6d7110f8c3ea9bbc514f3e560dc001780bbbd7a
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-15 22:30:54 +0900

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import { DatePicker, TimePicker, DateTimePicker } from "@material-ui/pickers";
+import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
 
 export const App = () => {
-  const [selectedDate, handleDateChange] = React.useState(new Date());
+  const [selectedDate, handleDateChange] = React.useState<
+    MaterialUiPickersDate
+  >(new Date() as any);
 
   return (
     <div>
-      <DatePicker value={selectedDate} onChange={handleDateChange} />
-      <TimePicker value={selectedDate} onChange={handleDateChange} />
-      <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+      <DatePicker
+        value={selectedDate}
+        onChange={(date) => {
+          handleDateChange(date);
+        }}
+        shouldDisableDate={(day: MaterialUiPickersDate) => {
+          if (!day) throw new Error("");
+          return day.weekday === 1 || day.weekday === 7;
+        }}
+      />
     </div>
   );
 };
